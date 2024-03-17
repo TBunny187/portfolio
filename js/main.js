fe0b45a80703235b3992f1f76e3cfbc7b43ac282
Sean Walters <111199616+TBunny187@users.noreply.github.com>
2024-03-17 00:16:06 -0400

    Fir problem with slideshow picture not showing

--- js/main.js
+++ js/main.js
@@ -7,7 +7,21 @@ function closeForm() {
 }
 
 let slideIndex = 1;
-showSlides(slideIndex);
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName('mySlides');
+    let dots = document.getElementsByClassName('dot');
+    if(n > slides.length) {slideIndex = 1};
+    if(n < 1) {slideIndex = slides.length};
+    for(let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    };
+    for(let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    };
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+}
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -17,20 +31,7 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-    let slides = document.getElementsByClassName('mySlides');
-    let dots = document.getElementsByClassName('dot');
-    if(n > slides.length) {slideIndex = 1};
-    if(n < 1) {slideIndex = slides.length};
-    for(let i = 0; i < slides.length; i++) {
-        slides[i].styles.display = 'none';
-    }
-    for(let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(' active', '');
-    }
-    slides[slideIndex - 1].display = 'block';
-    dots[slideIndex - 1].className += ' active';
-}
+showSlides(slideIndex);
 
 document.addEventListener('click', function(event) {
     if(event.target.matches('.cancel') || !event.target.closest('.form-popup') && !event.target.closest('.popUpButton') && !event.target.closest('.contact')) {
